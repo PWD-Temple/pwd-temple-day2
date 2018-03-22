8350ab8442506d1a66cf609dc81a55411146ad62
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-03-22 08:57:27 -0400

    first commit

---
+++ twitter_key.py
@@ -0,0 +1,4 @@
+api_key = "put your api key here between the quotes"
+api_secret = "put your secret key here between the quotes"
+api_key = "ufTztjyELlCDXrNqFbxEFDVhV"
+api_secret = "RYFd2pALNVD1gWdDd28ebdLSXHCP9gQu0rRJzJPes472x8xVfS"
